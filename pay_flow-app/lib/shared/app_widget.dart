82369f397aac705a58a4ade91074bf9284d8d7f2
Gonzaga <luizgonzagabn@gmail.com>
2021-06-27 21:27:26 -0300

    commit

--- pay_flow-app/lib/shared/app_widget.dart
+++ pay_flow-app/lib/shared/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pay_flow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:pay_flow/modules/home/home_page.dart';
 import 'package:pay_flow/modules/login/login_page.dart';
@@ -7,6 +8,13 @@ import 'package:pay_flow/modules/splash/splash_page.dart';
 import 'themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
